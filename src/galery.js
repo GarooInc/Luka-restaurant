7bc05baec5bb700eec029781e3409a49aaef9274
Jimena Hernández <her21199@uvg.edu.gt>
2023-09-11 12:52:19 -0600

    Update galery.js

--- src/galery.js
+++ src/galery.js
@@ -27,102 +27,102 @@ export const galeryImages = [
     {
         id: 6,
         src: "/images/galery/6.jpg",
-        alt: "1"
+        alt: "6"
     },
     {
         id: 7,
         src: "/images/galery/7.jpg",
-        alt: "2"
+        alt: "7"
     },
     {
         id: 8,
         src: "/images/galery/8.jpg",
-        alt: "3"
+        alt: "8"
     },
     {
         id: 9,
         src: "/images/galery/9.jpg",
-        alt: "4"
+        alt: "9"
     },
     {
         id: 10,
         src: "/images/galery/10.jpg",
-        alt: "5"
+        alt: "10"
     },
     {
         id: 11,
         src: "/images/galery/11.jpg",
-        alt: "1"
+        alt: "11"
     },
     {
         id: 12,
         src: "/images/galery/12.jpg",
-        alt: "2"
+        alt: "12"
     },
     {
         id: 13,
         src: "/images/galery/13.jpg",
-        alt: "3"
+        alt: "13"
     },
     {
         id: 14,
         src: "/images/galery/14.jpg",
-        alt: "4"
+        alt: "14"
     },
     {
         id: 15,
         src: "/images/galery/15.jpg",
-        alt: "5"
+        alt: "15"
     },
     {
         id: 16,
         src: "/images/galery/16.jpg",
-        alt: "1"
+        alt: "16"
     },
     {
         id: 17,
         src: "/images/galery/17.jpg",
-        alt: "2"
+        alt: "17"
     },
     {
         id: 18,
         src: "/images/galery/18.jpg",
-        alt: "3"
+        alt: "18"
     },
     {
         id: 19,
         src: "/images/galery/19.jpg",
-        alt: "4"
+        alt: "19"
     },
     {
         id: 20,
         src: "/images/galery/20.jpg",
-        alt: "5"
+        alt: "20"
     },
     {
         id: 21,
         src: "/images/galery/21.jpg",
-        alt: "1"
+        alt: "21"
     },
     {
         id: 22,
         src: "/images/galery/22.jpg",
-        alt: "2"
+        alt: "22"
     },
     {
         id: 23,
         src: "/images/galery/23.jpg",
-        alt: "3"
+        alt: "23"
     },
     {
         id: 24,
         src: "/images/galery/24.jpg",
-        alt: "4"
+        alt: "24"
     },
     {
         id: 25,
         src: "/images/galery/25.jpg",
-        alt: "5"
+        alt: "25"
     }
 ]
 
