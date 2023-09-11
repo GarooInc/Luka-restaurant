1a5c5363a895c6411022299a09ec2b1907fa9e1e
Jimena Hernández <her21199@uvg.edu.gt>
2023-09-11 11:34:53 -0600

    update pictures

--- src/galery.js
+++ src/galery.js
@@ -23,6 +23,106 @@ export const galeryImages = [
         id: 5,
         src: "/images/galery/5.jpg",
         alt: "5"
+    },
+    {
+        id: 6,
+        src: "/images/galery/6.jpg",
+        alt: "1"
+    },
+    {
+        id: 7,
+        src: "/images/galery/7.jpg",
+        alt: "2"
+    },
+    {
+        id: 8,
+        src: "/images/galery/8.jpg",
+        alt: "3"
+    },
+    {
+        id: 9,
+        src: "/images/galery/9.jpg",
+        alt: "4"
+    },
+    {
+        id: 10,
+        src: "/images/galery/10.jpg",
+        alt: "5"
+    },
+    {
+        id: 11,
+        src: "/images/galery/11.jpg",
+        alt: "1"
+    },
+    {
+        id: 12,
+        src: "/images/galery/12.jpg",
+        alt: "2"
+    },
+    {
+        id: 13,
+        src: "/images/galery/13.jpg",
+        alt: "3"
+    },
+    {
+        id: 14,
+        src: "/images/galery/14.jpg",
+        alt: "4"
+    },
+    {
+        id: 15,
+        src: "/images/galery/15.jpg",
+        alt: "5"
+    },
+    {
+        id: 16,
+        src: "/images/galery/16.jpg",
+        alt: "1"
+    },
+    {
+        id: 17,
+        src: "/images/galery/17.jpg",
+        alt: "2"
+    },
+    {
+        id: 18,
+        src: "/images/galery/18.jpg",
+        alt: "3"
+    },
+    {
+        id: 19,
+        src: "/images/galery/19.jpg",
+        alt: "4"
+    },
+    {
+        id: 20,
+        src: "/images/galery/20.jpg",
+        alt: "5"
+    },
+    {
+        id: 21,
+        src: "/images/galery/21.jpg",
+        alt: "1"
+    },
+    {
+        id: 22,
+        src: "/images/galery/22.jpg",
+        alt: "2"
+    },
+    {
+        id: 23,
+        src: "/images/galery/23.jpg",
+        alt: "3"
+    },
+    {
+        id: 24,
+        src: "/images/galery/24.jpg",
+        alt: "4"
+    },
+    {
+        id: 25,
+        src: "/images/galery/25.jpg",
+        alt: "5"
     }
 ]
 
